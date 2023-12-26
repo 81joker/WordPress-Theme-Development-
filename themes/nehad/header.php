c7756d1f790e415589388c9f05b1a57802c260a7
Nehad <35563834+81joker@users.noreply.github.com>
2023-12-26 12:56:23 +0100

    first commit

---
+++ themes/nehad/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html <?php get_language_attributes( 'en' ) ?>>
+<head>
+    <meta charset="<?php bloginfo('charset') ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adavanced Wordpress</title>
+</head>
+<body>
+    <h1>Navbar</h1>
---
+++ themes/nehad/index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
+
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+?>
+
+    <?php wp_head() ?>
+    <?php get_header();?>
+    <h1>Halloyxyxy≈a<y<</h1>
+    <?php wp_footer() ?>
+</body>
+</html>
